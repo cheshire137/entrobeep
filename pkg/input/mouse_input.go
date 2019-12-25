7341dd5a81b106e3aa748be677196b09afb7717b
Sarah Vessels <cheshire137@gmail.com>
2019-12-25 11:47:18 -0600

    Don't change x/y in mouse input

--- pkg/input/mouse_input.go
+++ pkg/input/mouse_input.go
@@ -13,15 +13,9 @@ var _ Input = (*MouseInput)(nil)
 
 func (i *MouseInput) Get() int {
 	x, y := robotgo.GetMousePos()
-	randomNum := rand.Intn(4)
+	randomNum := rand.Intn(2)
 	if randomNum < 1 {
 		return x
 	}
-	if randomNum < 2 {
-		return y
-	}
-	if randomNum < 3 {
-		return x + y
-	}
-	return x * y // if randomNum == 3
+	return y
 }
